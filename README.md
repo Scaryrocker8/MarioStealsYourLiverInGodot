3054d77728d49142c39f14b33f9abf6efac416c3
Jonnie <scaryrocker8@yahoo.com>
2026-01-30 20:30:38 -0600

    working on system tray functionality

--- README.md
+++ README.md
@@ -6,8 +6,22 @@ Running is not an option.
 
 You have 3 days to live.
 
-The classic meme recreated in Godot!
+The classic meme recreated and expanded in Godot!
 
-![screenshot](preview1.png)
+# v1.0.0
+
+First release of Mario Steals You Liver In Godot!
+
+# v1.0.1
+
+Added an authentic 3 day countdown until Mario steals your liver!
+
+When closing the application, it minimizes Mario to your system tray.
+
+In 24 hours after application has started Mario will reappear, saying you now only have 2 days until he steals your liver.
+
+Mario will continue counting down each day until day 0.
+
+After that, I don't know what happens.
 
 Created by Jonnie Gieringer
--- main.gd
+++ main.gd
@@ -1,6 +1,9 @@
 extends Control
 class_name Main
 
+const DAY: int = 86400
+const DAY_TEST: int = 60
+
 # Buttons
 @export var ok_button: Button
 @export var cancel_button: Button
@@ -9,11 +12,37 @@ class_name Main
 @export var mario_audio: AudioStreamPlayer
 @export var mario_sounds: Array[AudioStreamWAV]
 
+var wake_time = Time.get_unix_time_from_system()# + DAY
+
 func _ready() -> void:
+	# Setup system tray
+	var status_indicator: StatusIndicator = StatusIndicator.new()
+	status_indicator.icon = load("res://assets/trayicon.png")
+	status_indicator.tooltip = "You can't escape."
+	add_child(status_indicator)
+	
+	var menu: PopupMenu = PopupMenu.new()
+	add_child(menu)
+	menu.add_item("Show Mario", 1)
+	status_indicator.menu = menu.get_path()
+	menu.id_pressed.connect(_on_menu_item_pressed)
+	
+	get_tree().set_auto_accept_quit(false)
+
 	# Mario tells you you have 3 days until he steals your liver
 	ok_button.grab_focus()
 	mario_audio.stream = mario_sounds[0]
 	mario_audio.play()
+
+func _process(_delta: float) -> void:
+	print(Time.get_unix_time_from_system())
+
+func _on_menu_item_pressed(id: int) -> void:
+	if id == 1:
+		DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_NO_FOCUS, false)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+#region Button Callbacks
 
 func _on_cancel_pressed() -> void:
 	# Mario takes away option to cancel.
@@ -24,5 +53,8 @@ func _on_cancel_pressed() -> void:
 	mario_audio.play()
 
 func _on_ok_pressed() -> void:
-	# Mario gives you 3 days to live.
-	get_tree().quit()
+	# Minimize to tray
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_NO_FOCUS, true)
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MINIMIZED)
+
+#endregion
